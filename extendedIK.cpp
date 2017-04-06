fd730b127108c3d068c49194c461bb17035dea17
Luca <tagliapietra@gest.unipd.it>
2017-04-06 17:13:49 +0200

    Refactory for main of extendedIK

---
+++ extendedIK.cpp
@@ -0,0 +1,121 @@
+#include <OpenSim/OpenSim.h>
+#include "OpenSim/Tools/InverseKinematicsExtendedTool.h"
+
+using namespace std;
+
+static void PrintUsage(const char *aProgName, ostream &aOStream);
+
+int main(int argc, char **argv) {
+
+  // Set output formats
+  OpenSim::IO::SetDigitsPad(4);
+
+  // Register types
+  OpenSim::InverseKinematicsExtendedTool::registerTypes();
+
+  string setupFilename;
+  string option = "";
+  bool useVisualizer = false;
+
+  // No option provided, print usage options
+  if (argc < 2) {
+    PrintUsage(argv[0], cout);
+    exit(EXIT_FAILURE);
+  }
+
+  // Parse command line arguments
+  else {
+    for (int i = 1; i <= (argc - 1); i++) {
+      option = argv[i];
+
+      // Print usage options
+      if ((option == "-help") || (option == "-h") || (option == "-Help") || (option == "-H") ||
+          (option == "-usage") || (option == "-u") || (option == "-Usage") || (option == "-U")) {
+        PrintUsage(argv[0], cout);
+        exit(EXIT_SUCCESS);
+      }
+
+      // Identify the setup file
+      else if ((option == "-S") || (option == "-Setup")) {
+        if (argv[i + 1] == 0) {
+          PrintUsage(argv[0], cout);
+          exit(EXIT_FAILURE);
+        }
+        setupFilename = argv[i + 1];
+        ++i;
+        continue;
+      }
+
+      // Set if extendedIK uses Simbody visualizer
+      else if ((option == "-V") || (option == "-UseVisualizer")) {
+        useVisualizer = true;
+        continue;
+      }
+
+      // Print a default setup file
+      else if ((option == "-PrintSetup") || (option == "-PS")) {
+        OpenSim::InverseKinematicsExtendedTool *ikTool = new OpenSim::InverseKinematicsExtendedTool();
+        ikTool->setName("default");
+
+        // Add in useful objects that may need to be instantiated
+        OpenSim::Object::setSerializeAllDefaults(true);
+        ikTool->print("default_Setup_Extended_Inverse_Kinematics.xml");
+        OpenSim::Object::setSerializeAllDefaults(false);
+        cout << "Created file default_Setup_Extended_Inverse_Kinematics.xml with default setup" << endl;
+        delete ikTool;
+        exit(EXIT_SUCCESS);
+      }
+
+      // Print property info
+      else if ((option == "-PropertyInfo") || (option == "-PI")) {
+        if ((i + 1) >= argc)
+          OpenSim::Object::PrintPropertyInfo(cout, "");
+        else {
+          char *compoundName = argv[i + 1];
+          if (compoundName[0] == '-')
+            OpenSim::Object::PrintPropertyInfo(cout, "");
+          else
+            OpenSim::Object::PrintPropertyInfo(cout, compoundName);
+        }
+        exit(EXIT_SUCCESS);
+      }
+      // Unrecognized
+      else {
+        cout << "Unrecognized option " << option << " on command line... Ignored" << endl;
+        PrintUsage(argv[0], cout);
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+
+  // Create and run tool
+  try {
+    OpenSim::InverseKinematicsExtendedTool ikTool(setupFilename);
+
+    ikTool.setUseVisualizer(useVisualizer);
+    ikTool.run();
+
+    exit(EXIT_SUCCESS);
+  }
+  catch (const OpenSim::Exception& x) {
+    x.print(cout);
+    exit(EXIT_FAILURE);
+  }
+}
+
+
+//_____________________________________________________________________________
+/**
+ * Print the usage for this application
+ */
+void PrintUsage(const char *aProgName, ostream &aOStream) {
+  string progName = OpenSim::IO::GetFileNameFromURI(aProgName);
+  aOStream << "\n\n" << progName << "\n\n";
+  aOStream << "Option                Argument          Action / Notes\n";
+  aOStream << "------                --------          --------------\n";
+  aOStream << "-Help, -H                               Print the command-line options for " << progName << ".\n";
+  aOStream << "-PrintSetup, -PS                        Generates a template Setup file to customize extended IK. \n";
+  aOStream << "-Setup, -S            SetupFileName     Specify an xml setup file for extended IK.\n";
+  aOStream << "-UseVisualizer, -V                      Use Simbody visualizer. To be used together with -S option.\n";
+  aOStream << "-PropertyInfo, -PI                      Print help information for properties in setup files.\n";
+}
